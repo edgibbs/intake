324eddb6e70fcdc38a63adc3a05843692651e0c1
Nate Jones <Nate.Jones@OSI.ca.gov>
2017-08-31 15:02:34 -0700

    Upgrade webpacker
    change logging in test environment to use
    TaggedLogging. Webpack 3.0 currently call tag on our logger
    while compiling assets

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,6 +38,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
